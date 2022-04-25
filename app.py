0be9f27f4c12993de3727f87fec2a6ab23fa9b23
Leonie Koch <44017144+l30ni3@users.noreply.github.com>
2022-04-25 16:30:28 -0500

    add api request

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
+import requests
 
 app = Dash(__name__)
+
+gov_requests = requests.get(
+    "https://www.govdata.de/ckan/api/3/action/organization_list?all_fields=true&sort=package_count%20desc"
+)
+
+json_data = gov_requests.json()
+df = pd.DataFrame(json_data)
+print (df)
 
 app.layout = html.Div(children=[
     html.H1(children='GovData Dashboard'),
